4655b307904ca2248b6f41a47040323363a64ea5
Chen Sun <chenimal.sun@lalamove.com>
2017-08-06 21:29:52 +0800

    + color

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 # a command line tool to easily select db to connect
 import yaml
+from colorama import Fore
 from subprocess import call
 
 # load config file
@@ -7,21 +8,21 @@ f = open("/Users/chen/lalamove/data/db.yml")
 o = yaml.load(f)
 
 # select environment
-selected_env = input(o.keys())
+selected_env = input(Fore.GREEN + (',').join(list(o.keys())) + ": ")
 if selected_env not in o:
-    print("Too simple")
+    print(Fore.RED+ "Too simple")
     exit()
 
 # select projects
-selected_project = input(o[selected_env].keys())
+selected_project = input(Fore.GREEN + (',').join(list(o[selected_env].keys())) + ": ")
 if selected_project not in o[selected_env]:
-    print("Sometimes naive")
+    print(Fore.RED+ "Sometimes naive")
     exit()
 
 # select city
-selected_city = input(o[selected_env][selected_project].keys())
+selected_city = input(Fore.GREEN + (',').join(list(o[selected_env][selected_project].keys())) + ": ")
 if selected_city not in o[selected_env][selected_project]:
-    print("Naive")
+    print(Fore.RED+ "Naive")
     exit()
 
 call(o[selected_env][selected_project][selected_city].split(' '))
