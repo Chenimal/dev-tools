6359be9b52b404a7415845a6510626ce4f07f3e5
Chen Sun <chenimal.sun@lalamove.com>
2017-08-08 09:55:03 +0800

    * update error msg

--- db.py
+++ db.py
@@ -10,13 +10,13 @@ o = yaml.load(f)
 # select environment
 selected_env = input(Fore.GREEN + (',').join(list(o.keys())) + ": ")
 if selected_env not in o:
-    print(Fore.RED+ "Too simple")
+    print(Fore.RED+ "Too young")
     exit()
 
 # select projects
 selected_project = input(Fore.GREEN + (',').join(list(o[selected_env].keys())) + ": ")
 if selected_project not in o[selected_env]:
-    print(Fore.RED+ "Sometimes naive")
+    print(Fore.RED+ "Too simple")
     exit()
 
 cities = o[selected_env][selected_project]
@@ -29,7 +29,7 @@ if len(cities) == 1:
 # select city
 selected_city = input(Fore.GREEN + (',').join(list(cities.keys())) + ": ")
 if selected_city not in cities:
-    print(Fore.RED+ "Naive")
+    print(Fore.RED+ "Some times naive")
     exit()
 
 print(Fore.WHITE+"Connecting DB...")
