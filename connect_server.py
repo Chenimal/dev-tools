3dd3650a8176653f2fa4d15f087e96d9db7de5f0
Chenimal Sun <chenimal.sun@lalamove.com>
2017-09-18 09:30:01 +0800

    + connect to server

---
+++ connect_server.py
@@ -0,0 +1,21 @@
+# script to connect to aws server
+import yaml
+from colorama import Fore
+from subprocess import call
+
+data_path = "/Users/chen/lalamove/data/"
+
+f = open(data_path + "server.yml")
+o = yaml.load(f)
+
+if len(o) == 1:
+    selected_env = list(o.keys())[0]
+else:
+    selected_env = input(Fore.YELLOW + (',').join(list(o.keys())) + ": ", Fore.WHITE)
+    if selected_env not in o:
+        print(Fore.RED + "Too young")
+        exit()
+
+env = o[selected_env]
+call(["ssh", "-i", data_path + env['cert'], "ubuntu@" + env['host']])
+exit()
